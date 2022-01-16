e5d7e98295a1a66328d23ac09fe6724ebd816636
Sergey <zubby749@gmail.com>
2022-01-16 06:52:43 +0300

    update help window and info window

--- src/main/java/com/example/mega_sudoku/HelloController.java
+++ src/main/java/com/example/mega_sudoku/HelloController.java
@@ -1,31 +1,38 @@
 package com.example.mega_sudoku;
 
-import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
     @FXML
     protected Button devInfoButton, helpButton;
 
     @FXML
-    protected void onHelloButtonClick() throws IOException {
-        Stage stage = (Stage) devInfoButton.getScene().getWindow();
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("dev_info_screen.fxml")));
+    protected void onInfoButtonClick() throws IOException {
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UTILITY);
+        stage.initStyle(StageStyle.UTILITY);
+        stage.setResizable(false);
+        Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("dev_info_screen.fxml"))));
         stage.setScene(scene);
         stage.show();
     }
 
     @FXML
     protected void onHelpButtonClick() throws IOException {
-        Stage stage = (Stage) helpButton.getScene().getWindow();
-        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("help_screen.fxml")));
+        Stage stage = new Stage();
+        stage.initStyle(StageStyle.UTILITY);
+        stage.setResizable(false);
+        stage.setOnCloseRequest(Event::consume);
+        Scene scene = new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("help_screen.fxml"))));
         stage.setScene(scene);
         stage.show();
     }
