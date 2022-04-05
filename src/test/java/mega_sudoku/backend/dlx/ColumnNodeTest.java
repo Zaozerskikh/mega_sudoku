01c215ba9e9412e32a186c323902939720bfe0e5
nikitazimovnov <nikita.zimovnov@mail.ru>
2022-04-05 04:30:32 +0300

    Tests for generated problem grid.

---
+++ src/test/java/mega_sudoku/backend/dlx/ColumnNodeTest.java
@@ -0,0 +1,23 @@
+package mega_sudoku.backend.dlx;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColumnNodeTest {
+    ColumnNode C;
+
+    @BeforeEach
+    void setUp() {
+        C = new ColumnNode();
+    }
+
+    @Test
+    void cover() {
+    }
+
+    @Test
+    void uncover() {
+    }
+}
